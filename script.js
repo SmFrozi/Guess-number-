4b6e1bc97b8e97d931e364525a371993e5399b90
Deividas <xikdron9@gmail.com>
2026-03-01 14:45:56 +0200

    final version guess my number

--- script.js
+++ script.js
@@ -1,12 +1,49 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random() * 20 + 1);
-const score = 20;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20; 
+let hightscore = 0;
+
+const displayMessage = function(message) {
+  document.querySelector('.message').textContent = message;
+}
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-
   console.log(guess);
+  if (!guess) {
+    displayMessage("Error!");
+  } else if (guess === secretNumber) {
+    displayMessage("You won!");
+    document.querySelector('body').style.backgroundColor = '#3eaa1e';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
+    if (score > hightscore) {
+      hightscore = score;
+      document.querySelector('.highscore').textContent = score;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? 'Меньш' : 'Больше');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage("You lose!");
+      document.querySelector(".score").textContent = 0;
+      document.querySelector('body').style.backgroundColor = '#991c1c';
+    }
+  } 
 });
 
-document.querySelector('.again').addEventListener('click', function () {});
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
